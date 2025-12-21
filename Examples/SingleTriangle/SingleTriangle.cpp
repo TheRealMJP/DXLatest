032e27f57b33801f9d55e69c2c420fb9833efa46
MJP <mpettineo@gmail.com>
2025-12-21 15:59:34 -0800

    Initial repo structure with an example project

---
+++ Examples/SingleTriangle/SingleTriangle.cpp
@@ -0,0 +1,6 @@
+#include "../../dxlatest.h"
+#include <windows.h>
+
+int32_t APIENTRY WinMain([[maybe_unused]] HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance, [[maybe_unused]] char* lpCmdLine, [[maybe_unused]] int32_t nCmdShow)
+{
+}
---
+++ dxlatest.cpp
@@ -0,0 +1,5 @@
+#include "AgilitySDK/include/d3d12.h"
+
+#ifdef _MSC_VER
+#pragma comment(lib, "D3D12.lib")
+#endif
