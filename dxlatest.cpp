488c62f30da2f030d247eaa7299ed24a4dfe27d4
MJP <mpettineo@gmail.com>
2025-12-21 16:36:19 -0800

    IDXLBase

--- dxlatest.cpp
+++ dxlatest.cpp
@@ -1,5 +1,69 @@
+#include "dxlatest.h"
+
 #include "AgilitySDK/include/d3d12.h"
 
 #ifdef _MSC_VER
 #pragma comment(lib, "D3D12.lib")
 #endif
+
+namespace dxl
+{
+
+// == DXLBase ======================================================
+
+HRESULT DXLBase::QueryInterface(REFIID riid, void** outObject)
+{
+    return underyling->QueryInterface(riid, outObject);
+}
+
+ULONG DXLBase::AddRef()
+{
+    return ++refCount;
+}
+
+ULONG DXLBase::Release()
+{
+    if (refCount == 1)
+    {
+        underyling->Release();
+        return 0;
+    }
+    else
+    {
+        return --refCount;
+    }
+}
+
+// == IDXLObject ======================================================
+
+HRESULT IDXLObject::GetPrivateData(REFGUID guid, uint32_t* outDataSize, void* outData)
+{
+    return ToID3D12Object()->GetPrivateData(guid, outDataSize, outData);
+}
+
+HRESULT IDXLObject::SetPrivateData(REFGUID guid, uint32_t dataSize, const void *data)
+{
+    return ToID3D12Object()->SetPrivateData(guid, dataSize, data);
+}
+
+HRESULT IDXLObject::SetPrivateDataInterface(REFGUID guid, const IUnknown* data)
+{
+    return ToID3D12Object()->SetPrivateDataInterface(guid, data);
+}
+
+HRESULT IDXLObject::SetName(const wchar_t* name)
+{
+    return ToID3D12Object()->SetName(name);
+}
+
+/*HRESULT IDXLObject::SetName(const char* name)
+{
+    return ToID3D12Object()->SetName(name);
+}*/
+
+ID3D12Object* IDXLObject::ToID3D12Object() const
+{
+    return reinterpret_cast<ID3D12Object*>(underyling);
+}
+
+} // namespace dxl
--- dxlatest.h
+++ dxlatest.h
@@ -2,7 +2,41 @@
 
 #include <stdint.h>
 
+#define COM_NO_WINDOWS_H
+#include <Unknwn.h>
+
+// Forward declarations
+struct ID3D12Object;
+
 namespace dxl
 {
 
-}
+class DXLBase : public IUnknown
+{
+
+public:
+
+    HRESULT QueryInterface(REFIID riid, void** outObject) override;
+    ULONG AddRef() override;
+    ULONG Release() override;
+
+protected:
+
+    IUnknown* underyling = nullptr;
+    ULONG refCount = 1;
+};
+
+class IDXLObject : public DXLBase
+{
+
+public:
+
+    HRESULT GetPrivateData(REFGUID guid, uint32_t* outDataSize, void* outData);
+    HRESULT SetPrivateData(REFGUID guid, uint32_t dataSize, const void *data);
+    HRESULT SetPrivateDataInterface(REFGUID guid, const IUnknown* data);
+    HRESULT SetName(const wchar_t* name);
+    // HRESULT SetName(const char* name);
+    ID3D12Object* ToID3D12Object() const;
+};
+
+} // namespace dxl
